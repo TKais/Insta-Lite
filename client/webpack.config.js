6fe57da0a4dbcd7edf1c5be56b3ad5c632cbcd51
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-04-24 11:12:41 -0700

    Update webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|.jsx/,
+        loader: 'babel-loader',
+        exlude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src'),
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
